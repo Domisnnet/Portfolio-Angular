efb31871110932ad473954004953583adff9b0fe
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-03-12 17:19:45 +0000

    ajustados

--- src/app/services/stack.service.ts
+++ src/app/services/stack.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { STACK_CONFIG, PillCategory, TagKey } from '../constants/project-tags.config';
-import { PERSONAL_STACK } from '../constants/personal-stack.config';
+import { STACK_CONFIG, PillCategory, TagKey } from '@app/constants/project-tags.config';
+import { PERSONAL_STACK } from '@app/constants/personal-stack.config';
 
 @Injectable({ providedIn: 'root' })
 export class StackService {
