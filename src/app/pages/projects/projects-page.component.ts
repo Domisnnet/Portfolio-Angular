c5e3442932f9cfc2144067f49e23be2ee7a4ce08
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-03-23 19:00:38 +0000

    ok

--- src/app/pages/projects/projects-page.component.ts
+++ src/app/pages/projects/projects-page.component.ts
@@ -82,7 +82,7 @@ export class ProjectsPageComponent {
       description: 'Portfólio animado com narrativa visual.',
       image: 'assets/images/portfólio.png',
       tags: ['angular', 'scss', 'motions', 'design'],
-      link: 'https://github.com/Portfolio',
+      link: 'https://github.io.com/Portfolio',
     },
   ]);
 }
