20b2391fa1eecbfd6e7fae98a147ad07e70915b1
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-06-03 15:39:32 +0000

    ok

---
+++ .angular/cache/20.3.27/portfolio/vite/deps/index.esm-X5LYXJIG.js
@@ -0,0 +1,5 @@
+import "./chunk-5QEL5FPV.js";
+import "./chunk-DFHPHJHW.js";
+import "./chunk-7CKLBN5A.js";
+import "./chunk-B2KI3AIV.js";
+import "./chunk-653SOEEV.js";
--- .angular/cache/21.0.5/portfolio/vite/deps/index.esm-RAFYFHAH.js
+++
@@ -1,6 +0,0 @@
-import "./chunk-J24ADAPB.js";
-import "./chunk-M3RNIKZN.js";
-import "./chunk-JAKQQ4IW.js";
-import "./chunk-WXWJ24DR.js";
-import "./chunk-B2KI3AIV.js";
-import "./chunk-653SOEEV.js";
