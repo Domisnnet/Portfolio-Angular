be5fecc60528a5b3a0324df5bf7b82939a218019
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-06-03 10:27:15 +0000

    ok

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from '@app/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   email = '';
